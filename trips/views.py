aef4e33987c285ce5efa834d482e591514205667
asbenjamin <shammahbenjamin@gmail.com>
2022-12-09 14:37:27 +0300

    Add login tests and views

--- trips/views.py
+++ trips/views.py
@@ -1,9 +1,14 @@
 from django.contrib.auth import get_user_model
 from rest_framework import generics
 
-from .serializers import UserSerializer
+from .serializers import UserSerializer, LoginSerializer
+from rest_framework_simplejwt.views import TokenObtainPairView # new
 
 
 class SignUpView(generics.CreateAPIView):
     queryset = get_user_model().objects.all()
     serializer_class = UserSerializer
+
+
+class LoginView(TokenObtainPairView): # new
+    serializer_class = LoginSerializer
